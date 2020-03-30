c3c806ce22aaa0d9b795460e8c4c4e80bb763dae
Arnau <arnau27@pm.me>
2020-03-30 18:04:56 +0200

    Auth Implementation

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,3 +1,13 @@
 import express from 'express';
 const router = express.Router();
-const Race = require('../models/User');
+const User = require('../models/User');
+
+ router.post('/register', (req, res) => {
+     const user = new User({
+         Username: req.body.Username,
+         Email: req.body.Email,
+         Password: req.body.Password,
+         Photo: req.body.Photo,
+         Rithm: req.body.Rithm
+     });
+ })
