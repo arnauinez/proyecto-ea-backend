7acb235c9d8d84b68d3a925996d096e3ab1caab2
AntonEstrela <anton.estrela@gmail.com>
2020-04-06 19:40:47 +0200

    db form env

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,8 @@ app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Connect to DB
 mongoose.connect( 
     //'mongodb+srv://runnea.tk:27017/test0',
-    'mongodb+srv://dbuser:dbpass@cluster0-8hrhz.mongodb.net/test?retryWrites=true&w=majority',
+    //'mongodb+srv://dbuser:dbpass@cluster0-8hrhz.mongodb.net/test?retryWrites=true&w=majority',
+    String(process.env.DB_CONNECTION),
     { useNewUrlParser: true, useUnifiedTopology: true },
     () => { console.log('Connected to DB!')}
 );
