1ed1c27d2e6ad5e79e7cde1b2a4bcf1aa22258fe
Arnau <arnau27@pm.me>
2020-05-12 01:56:18 +0200

    First Oauth Approach

---
+++ src/routes/oauth.ts
@@ -0,0 +1,71 @@
+import express from 'express';
+require('dotenv').config();
+const router = express.Router();
+const passport = require("passport")
+const User = require('../models/User');
+// Strategies
+const FacebookStrategy = require("passport-facebook").Strategy
+
+router.use(passport.initialize())
+router.use(passport.session())
+
+passport.serializeUser(function(user: any, done: any) {
+  done(null, user)
+})
+passport.deserializeUser(function(user: any, done: any) {
+  done(null, user)
+})
+
+const fbOpts = {
+  clientID: process.env.FACEBOOK_CLIENT_ID,
+  clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  callbackURL: process.env.CALLBACK_URL,
+  profileFields: ["name", "email", "link", "locale"],
+  passReqToCallback: true
+}
+
+const fbCallback = async (accessToken: any, refreshToken: any, profile: any, req: any, done: any) => {
+  try {
+    console.log(profile); // UNDEFINED
+    await User.findOne({ Username: req.id },
+      async (err: Error, user: any) => {
+        if(err) {
+          console.log(err);  // handle errors!
+        }
+        if(user) {
+          console.log('> Logged In');
+        //Login if User already exists
+        } else {
+        // Register user in Local db
+          console.log('> Registered', req.id);
+          const user = new User({
+            Username: req.id
+          });
+        const savedUser = await user.save();
+        }
+      });
+
+    done(null, req);
+  } catch(err){
+    done(err, false, err.message);
+  }
+}
+passport.use(new FacebookStrategy(fbOpts, fbCallback));
+
+router.get('/', (req, res) => {
+  res.send('Got here');
+});
+
+router.get('/facebook', passport.authenticate('facebook'))
+router.get('/facebook/callback',
+  passport.authenticate('facebook', { 
+    successRedirect: '/oauth',
+    failureRedirect: '/' })
+  // (req: any, res: any) => {
+  //   console.log('req____', req);
+  //   res.send('HELLO')
+  // }
+)
+
+
+module.exports = router;
